a2ee89d8c9c83b24b27f165b9658fddfecb2fbef
Vladimir <snigirv@gmail.com>
2023-03-18 17:24:02 +0200

    chenged Task6

--- src/Homework16/Task6.java
+++ src/Homework16/Task6.java
@@ -7,6 +7,11 @@ public class Task6 {
         Scanner in1 = new Scanner(System.in);
         System.out.println("Вес метталла, который может храниться на складе");
         int maxWeight = in1.nextInt();
+        while (maxWeight <= 5) {System.out.println("Вес метталла на складе не можеты быть меньше 5");
+            System.out.println("Вес метталла, который может храниться на складе");
+        int maxWeight2 = in1.nextInt();
+        maxWeight = maxWeight2;}
+
         Scanner in2 = new Scanner(System.in);
         int countOfWeight =0;
 
@@ -16,16 +21,22 @@ public class Task6 {
             }
             System.out.println("Сколько сдать металла?");
             int weight = in2.nextInt();
-            if((countOfWeight+weight)>maxWeight) {
-                System.out.println("Такое количество металла не влезет");
-                System.out.println("Осталось места на "+(maxWeight-countOfWeight));
-                continue;
-            }
             if (weight<5) {
                 System.out.println("Невозможно принять такой малый вес");
                 continue;
             }
-            countOfWeight=countOfWeight+weight;
+            if((countOfWeight+weight)>maxWeight) {
+                System.out.println("Такое количество металла не влезет");
+                //System.out.println("Осталось места на "+(maxWeight-countOfWeight));
+                continue;
+            }
+            if ((countOfWeight+weight)>(maxWeight-4)&&(countOfWeight+weight)<(maxWeight-1)) {
+              System.out.println("Невозможно принять такую массу, так как склад не сможет затем принять менее 5!");
+              System.out.println("Осталось места на "+(maxWeight-countOfWeight));
+              continue;
+          };
+
+            countOfWeight = countOfWeight+weight;
             System.out.println("Осталось места на "+(maxWeight-countOfWeight));
         }
     }
