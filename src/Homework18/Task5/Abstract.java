cb7d62f756c9e0708c1542b3d86ded6e98983458
Vladimir <snigirv@gmail.com>
2023-03-22 00:14:56 +0200

    add Task5

---
+++ src/Homework18/Task5/Abstract.java
@@ -0,0 +1,49 @@
+package Homework18.Task5;
+
+public class Abstract {
+    public String fullName;
+    public String numberOfPages;
+    public int yearOfIssue;
+    public String coverColor;
+    public String nameOfInstitution;
+
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName (String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getNumberOfPages () {
+        return numberOfPages;
+    }
+    public void setNumberOfPages (String numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public int getYearOfIssue () {
+        return yearOfIssue;
+    }
+    public void setYearOfIssue (int yearOfIssue) {
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    public String getCoverColor () {
+        return coverColor;
+    }
+    public void setCoverColor (String coverColor) {
+        this.coverColor = coverColor;
+    }
+
+    public String getNameOfInstitution () {
+        return nameOfInstitution;
+    }
+    public void  setNameOfInstitution (String nameOfInstitution) {
+        this.nameOfInstitution = nameOfInstitution;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
